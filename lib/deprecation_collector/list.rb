eb588c9893485ba696c63b3df90e7f0009ec7222
Kelvin Tan <kelv@discourse.org>
2024-07-01 15:40:02 +0800

    Fixes to workflow and ember deprecations script

--- lib/deprecation_collector/list.rb
+++ lib/deprecation_collector/list.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
-DEPRECATION_IDS_FILE = 'lib/deprecation_collector/deprecation-ids.yaml'
 
+require "yaml"
 module DeprecationCollector
+  DEPRECATION_IDS_FILE = File.expand_path("../deprecation-ids.yml", __FILE__)
+
   deprecations = YAML.load_file(DEPRECATION_IDS_FILE)
-  List = (deprecations['ember_deprecation_ids'] || []).concat(deprecations['discourse_deprecation_ids'] || [])
+  List =
+    (deprecations["ember_deprecation_ids"] || []).concat(
+      deprecations["discourse_deprecation_ids"] || [],
+    )
 end
--- scripts/update_ember_deprecations.rb
+++ scripts/update_ember_deprecations.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
+require_relative "../lib/deprecation_collector/list"
 require "yaml"
 require "open-uri"
 
 begin
   deprecations = YAML.load_file(DeprecationCollector::DEPRECATION_IDS_FILE)
-  ember_deprecations = deprecations['ember_deprecation_ids'] || []
+  ember_deprecations = deprecations["ember_deprecation_ids"] || []
 
   system("git", "clone", "-q", "https://github.com/ember-learn/deprecation-app", "--depth", "1")
 
@@ -19,9 +20,9 @@ begin
 
   return if ember_deprecations.sort == deprecation_ids.sort
 
-  deprecations['ember_deprecation_ids'] = deprecation_ids
+  deprecations["ember_deprecation_ids"] = deprecation_ids
   File.write(DeprecationCollector::DEPRECATION_IDS_FILE, deprecations.to_yaml)
-  puts 'Updated ember deprecations'
+  puts "Updated ember deprecations"
 ensure
   system("rm", "-rf", "deprecation-app")
 end
